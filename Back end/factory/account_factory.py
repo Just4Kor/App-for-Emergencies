019c40ff43f40a34a8b3b6b6a0adb749e17a6afb
Just4Kor <justina.korsun@gmail.com>
2026-04-25 11:30:01 +0300

    Update account_factory.py

--- Back end/factory/account_factory.py
+++ Back end/factory/account_factory.py
@@ -1,31 +1,32 @@
-from werkzeug.security import generate_password_hash
+from models.user import User
+from models.worker import Worker
 
 
 class AccountFactory:
     @staticmethod
-    def create_customer(account_class, username, password, city):
-        return account_class(
-            role = "customer",
-            username = username,
-            password = generate_password_hash(password),
-            city = city,
+    def create_customer(user_id, username, password, city):
+        return User(
+            user_id=user_id,
+            username=username,
+            password=password,
+            city=city,
         )
 
     @staticmethod
     def create_worker(
-        account_class,
+        worker_id,
         username,
         password,
         specialty,
         location,
-        hourly_rate
+        hourly_rate,
     ):
-        return account_class(
-            role = "worker",
+        return Worker(
+            worker_id = worker_id,
             username = username,
-            password = generate_password_hash(password),
+            password = password,
             specialty = specialty,
             location = location,
             hourly_rate = hourly_rate,
-            rating = 5.0,
+            rating = "5",
         )
