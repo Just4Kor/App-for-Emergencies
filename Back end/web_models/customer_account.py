e894fad94834441fb90c416a2fde2aceed89d42c
Just4Kor <justina.korsun@gmail.com>
2026-04-25 11:30:48 +0300

    Refactor CustomerAccountModel to CustomerView

--- Back end/web_models/customer_account.py
+++ Back end/web_models/customer_account.py
@@ -1,10 +1,7 @@
-from web_models.account import BaseAccount
+from web_models.account import AccountView
 
 
-class CustomerAccountModel(BaseAccount):
-    def __init__(self, username, password, city):
-        super().__init__(username, password)
-        self.city = city
-
-    def get_role(self):
-        return "customer"
+class CustomerView(AccountView):
+    def __init__(self, customer):
+        super().__init__(customer)
+        self.city = customer.city
