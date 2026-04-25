5243277cb12c2eeda506f8701e55816b435ce6f9
Just4Kor <justina.korsun@gmail.com>
2026-04-25 11:28:36 +0300

    Refactor RatingService for rating calculations
    Refactor RatingService methods to simplify rating calculations and remove unused methods.

--- Back end/services/rating_service.py
+++ Back end/services/rating_service.py
@@ -1,26 +1,9 @@
 class RatingService:
-    def __init__(self):
-        self.ratings = []
+    def get_worker_rating(self, worker):
+        return worker.get_rating()
 
-    def add_rating(self, rating):
-        self.ratings.append(rating)
-        self.update_worker_average(rating.worker)
+    def calculate_average(self, scores):
+        if not scores:
+            return 5.0
 
-    def get_worker_ratings(self, worker):
-        return [
-            rating for rating in self.ratings
-            if rating.worker == worker
-        ]
-
-    def get_average_rating(self, worker):
-        worker_ratings = self.get_worker_ratings(worker)
-
-        if not worker_ratings:
-            return worker.rating
-
-        total = sum(rating.score for rating in worker_ratings)
-        return round(total / len(worker_ratings), 2)
-
-    def update_worker_average(self, worker):
-        average = self.get_average_rating(worker)
-        worker.update_rating(average)
+        return sum(scores) / len(scores)
