7eca8a6683d6319cb043a84cedab885dbcb4c18f
Just4Kor <justina.korsun@gmail.com>
2026-04-25 11:24:15 +0300

    Refactor Person class constructor and attributes

--- Back end/models/person.py
+++ Back end/models/person.py
@@ -2,9 +2,9 @@ from abc import ABC, abstractmethod
 
 
 class Person(ABC):
-    def __init__(self, person_id, name, location):
-        self.person_id = person_id
-        self.name = name
+    def __init__(self, person_id, username, location=""):
+        self.id = str(person_id)
+        self.username = username
         self.location = location
 
     @abstractmethod
