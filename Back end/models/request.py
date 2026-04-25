fe18a6de2c4074231698dbed8abbf58e51b4049e
Just4Kor <justina.korsun@gmail.com>
2026-04-25 11:27:07 +0300

    Enhance ServiceRequest with new attributes and methods
    Refactor ServiceRequest class to include additional attributes and methods.

--- Back end/models/request.py
+++ Back end/models/request.py
@@ -1,7 +1,33 @@
 from models.request_status import RequestStatus
 
 
+class ServiceRequest:from models.request_status import RequestStatus
+
+
 class ServiceRequest:
+    def __init__(
+        self,
+        request_id,
+        title,
+        description,
+        address,
+        status,
+        customer_id,
+        worker_id,
+    ):
+        self.id = str(request_id)
+        self.title = title
+        self.description = description
+        self.address = address
+        self.status = status or RequestStatus.PENDING
+        self.customer_id = str(customer_id)
+        self.worker_id = str(worker_id)
+
+    def accept(self):
+        self.status = RequestStatus.ACCEPTED
+
+    def deny(self):
+        self.status = RequestStatus.DENIED
     def __init__(
         self,
         request_id,
